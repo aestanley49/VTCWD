ea449628b13e2b988cb46d0e4a822cdd26a48a2e
aestanley49 <aestanley49@gmail.com>
2024-11-20 16:51:59 -0500

    set Jon's function as default function script and removed original This model now includes: - stable age structure - disease dynamics - stochasticity for population parameters

--- SII_model/functions/popnmodel_func.R
+++ SII_model/functions/popnmodel_func.R
@@ -1,4 +1,5 @@
 
+# Ref - https://clas.ucdenver.edu/marcelo-perraillon/sites/default/files/attached-files/lecture_12_inf_model.pdf
 
 ## Suseptible
 #' fawn.S.N = Fawn Suseptible count (scaler value >= 0),
@@ -70,9 +71,7 @@
 ## e = beta * no. infected * no susceptible + E
 ## i = probability of transitioning 
 
-#' e.fawn.rate = rate of fawns moving from susceptible to exposed (scaler value between 0 and 1),  
-#' 
-#' e.ja.rate = rate of juvenile and adults moving from susceptible to exposed (scaler value between 0 and 1),  
+#' e.rate = rate of all age classes moving from susceptible to exposed (scaler value between 0 and 1),  
 #' 
 #' i.rate = rate of all age classes moving from susceptible to exposed (scaler value between 0 and 1), 
 #' 
@@ -93,7 +92,16 @@
 #' 
 #' n0 = starting population value [we are assuming that no individuals are diseased when begin model?] 
 #' 
+#estimate beta distribution parameters from a mean and variance. 
+est_beta_params <- function(mu, var) {
+  # check that the var <mu(1-mu), if greater, use the largest possible
+  if(var > mu*(1-mu)){var = mu*(1-mu)-.00001}
+  alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
+  beta <- alpha * (1 / mu - 1)
+  return(params = list(alpha = alpha, beta = beta))
+}
 
+source("../VTCWD/SII_model/functions/CreateStableAgeStructure_func.R")
 
 
 cwd_stoch_model <- function(params) {
@@ -103,18 +111,6 @@ cwd_stoch_model <- function(params) {
   # write the list objects to the local environment
   for (v in 1:length(params)) assign(names(params)[v], params[[v]])
   
-  # check if static parameters exist.    
-  if(exists("fawn.an.sur")==FALSE){
-    message("fawn survival is missing, using default value")
-    fawn.an.sur <- 0.6
-  }
-  
-  if(exists("juv.f.an.sur")==FALSE){
-    message("juvenile survival is missing, using default value")
-    juv.f.an.sur <- 0.8
-  } 
-  
-  
   ### Create list to hold output for each year
   popout <- matrix(0, nrow = nyears, ncol = 14)
   dimnames(popout) <- list(as.character(seq(1:nyears)), c('fawn.S.N', 'juv.f.S.N', "juv.m.S.N", "ad.f.S.N", "ad.m.S.N", "fawn.E.N", "juv.f.E.N", "juv.m.E.N", "ad.f.E.N", "ad.m.E.N", "juv.f.I.N", "juv.m.I.N", "ad.f.I.N", "ad.m.I.N"))
@@ -122,134 +118,149 @@ cwd_stoch_model <- function(params) {
   # for hunted
   hunted <- popout
   
+  # for Environmental reservoir  
+  envres <- matrix(0, nrow = nyears, ncol = 1)
+  
   # CWD matrix 
   ### Need to set up a way to keep track of diseased individuals 
   
+  stablestate <- returnstartingpopn() # this is ordered fawns, juv f, adults f, juv m, adult m
   
-  ### Create starting population - copied from Paul Cross model. Is there a better way to do this???
-  # Create the Leslie Matrix to start the population at stable age dist
-  M <- matrix(rep(0, 5*5), nrow = 5) #fawn, juv f, adult f, juv m, adult m
+  popout[1,] <- c(stablestate[1:2], stablestate[4], stablestate[3], stablestate[5], rep(0, 9))
+  hunted[1,] <- c(popout[1,1] * hunt.mort.fawn, popout[1,2] * hunt.mort.juv.f, popout[1,3] * hunt.mort.juv.m, popout[1,4] * hunt.mort.ad.f, popout[1,5] * hunt.mort.ad.m, rep(0, 9))
+    
+
+
+  ##estimate vitals##
+  ### Survival
+  fawn.sur.beta <- est_beta_params(fawn.an.sur, fawn.an.sur.var)
+  juv.f.sur.beta <- est_beta_params(juv.f.an.sur, juv.f.an.sur.var)
+  juv.m.sur.beta <- est_beta_params(juv.m.an.sur, juv.m.an.sur.var)
+  ad.f.sur.beta <- est_beta_params(ad.an.f.sur, ad.an.f.sur.var)
+  ad.m.sur.beta <- est_beta_params(ad.an.m.sur, ad.an.m.sur.var)
+  ### Hunting 
+  hunt.fawn.beta <- est_beta_params(hunt.mort.fawn, hunt.var)
+  hunt.juv.f.beta <- est_beta_params(hunt.mort.juv.f, hunt.var)
+  hunt.juv.m.beta <- est_beta_params(hunt.mort.juv.m, hunt.var)
+  hunt.f.beta <- est_beta_params(hunt.mort.ad.f, hunt.var)
+  hunt.m.beta <- est_beta_params(hunt.mort.ad.m, hunt.var)
   
-  # replace the -1 off-diagonal with the survival rates
-  M[row(M) == (col(M) + 1)] <- c(juv.f.an.sur * (1 - hunt.mort.juv.f),
-                                 (ad.an.f.sur *  (1 - hunt.mort.ad.f)), 0,
-                                   (ad.an.m.sur * (1 - hunt.mort.ad.m)))
-  M[4,1] <-                     (juv.m.an.sur * (1 - hunt.mort.juv.m)) # don't have male and female fawns so this is wonky
-  # ****** Is this right??? 
-  
-  
-  # if you want the top age category to continue to survive
-  M[3, 3] <- ad.an.f.sur * (1 - hunt.mort.ad.f)
-  M[5, 5] <- ad.an.m.sur * (1 - hunt.mort.ad.m)
-  
-  # insert the fecundity vector prebirth census - fawns don't have sex so only doing this once
-  M[1, 1:3] <- c(0, juv.repro, ad.repro) * fawn.an.sur * (1 - hunt.mort.fawn)
-  
-  # Initializing with the stable age distribution.
-  suseptible <- round(popbio::stable.stage(M)[1:5] * n0) # fawns, juv f, adults f, juv m, adult m
-  
-  popout[1,] <- c(suseptible, rep(0, 9))
-  
+  ### Reproduction 
+  juv.r.beta <- est_beta_params(juv.repro/2, repro.var)
+  ad.r.beta <- est_beta_params(ad.repro/2, repro.var)
+  ### !!!!! Is this right?? Do we want to divide by 2??? 
   
   for(t in 2:nyears){
+    ## if there is too much inter annual variation, move this outside year
+    fawn.sur <- rbeta(1, fawn.sur.beta$alpha, fawn.sur.beta$beta)
+    juv.f.sur <- rbeta(1, juv.f.sur.beta$alpha, juv.f.sur.beta$beta)
+    juv.m.sur <- rbeta(1, juv.m.sur.beta$alpha, juv.m.sur.beta$beta)
+    ad.f.sur <- rbeta(1, ad.f.sur.beta$alpha, ad.f.sur.beta$beta)
+    ad.m.sur <- rbeta(1, ad.m.sur.beta$alpha, ad.m.sur.beta$beta)
     
-    #Pull out classes of individuals out of matrix and set as sex/age/disease state name
-    for (i in seq_along(colnames(popout))) {
-      col_names <- colnames(popout)
-      values <- popout[t-1,]
-      assign(col_names[i], values[i])
-    }
+    # stochastic hunting survival rates
+    hunt.fawn <- rbeta(1, hunt.fawn.beta$alpha, hunt.fawn.beta$beta, ncp = 0)
+    hunt.juv.f <- rbeta(1, hunt.juv.f.beta$alpha, hunt.juv.f.beta$beta, ncp = 0)
+    hunt.juv.m <- rbeta(1, hunt.juv.m.beta$alpha, hunt.juv.m.beta$beta, ncp = 0)
+    hunt.ad.f <- rbeta(1, hunt.f.beta$alpha, hunt.f.beta$beta, ncp = 0)
+    hunt.ad.m <- rbeta(1, hunt.m.beta$alpha, hunt.m.beta$beta, ncp = 0)
+    
+    # yearly stochastic reproductive rates
+    
+    
+    juv.repro.draw <- rbeta(1, juv.r.beta$alpha, juv.r.beta$beta, ncp = 0)
+    ad.repro.draw <- rbeta(1, ad.r.beta$alpha, ad.r.beta$beta, ncp = 0)
+
+    ### Disease dynamics
+    # transmission rate (or the rate of contact) beta and the probability of infection given that contact occurred
+
+    e.rate <- (sum(popout[t,-c(1:5)])/sum(popout[t,])) *R0
+    
+    envres[t,] <- envres[t-1,] + (popout[t,c(11:14)]*shedrate) - (envres[t-1,] * expdecayconstant)
+    
+    ## envres needs to feed into indirect transmission
+    # additive in beta? 
+    # contact rate and contact given exposure 
+    
     
     ### Susceptible Category 
     # Fawns
-    fawn.S.N <- (juv.f.S.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * (1-e.fawn.rate) ) + 
-      (ad.f.S.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * (1-e.fawn.rate) ) +
-      (juv.f.E.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * (1-e.fawn.rate) ) + 
-      ( ad.f.E.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * (1-e.fawn.rate) ) +
-      (juv.f.I.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * (1-e.fawn.rate) ) + 
-      ( ad.f.I.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * (1-e.fawn.rate) )
+    popout[t,1] <- (popout[t-1,2] * (1- hunt.juv.f) * juv.repro.draw * juv.f.sur * (1-e.rate) ) + 
+      (popout[t-1,4] * (1- hunt.ad.f) * ad.repro.draw * ad.f.sur * (1-e.rate)) +
+      (popout[t-1,7] * (1- hunt.juv.f) * juv.repro.draw * juv.f.sur * (1-e.rate) ) + 
+      (popout[t-1,9] * (1- hunt.ad.f) * ad.repro.draw * ad.f.sur * (1-e.rate)) +
+      (popout[t-1,11] * (1- hunt.juv.f) * juv.repro.draw * juv.f.sur * (1-e.rate) ) + 
+      (popout[t-1,13] * (1- hunt.ad.f) * ad.repro.draw * ad.f.sur * (1-e.rate)) 
     
     # Juveniles 
-    juv.f.S.N <- .5 * fawn.S.N * (1 - hunt.mort.fawn) * fawn.an.sur * (1 - e.fawn.rate)
+    popout[t,2] <- .5 * popout[t-1,1] * (1 - hunt.fawn) * fawn.sur * (1 - e.rate)
     
-    juv.m.S.N <- .5 * fawn.S.N * (1 - hunt.mort.fawn) * fawn.an.sur * (1 - e.fawn.rate)
-    
-    # Adults 
-    ad.f.S.N <- (ad.f.S.N * (1- hunt.mort.ad.f) * ad.an.f.sur * (1-e.ja.rate)) + 
-      (juv.f.S.N * (1 - hunt.mort.juv.f) * juv.f.an.sur * (1-e.ja.rate))
-    
-    ad.m.S.N <- (ad.m.S.N * (1- hunt.mort.ad.m) * (1-e.ja.rate)) + 
-      (juv.m.S.N * (1 - hunt.mort.juv.m) * juv.m.an.sur * (1-e.ja.rate))
-    
-    ### Exposed Category 
-    # Fawns
-    fawn.E.N <- (juv.f.S.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * e.fawn.rate ) + 
-      (ad.f.S.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * e.fawn.rate ) +
-      (juv.f.E.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * e.fawn.rate ) + 
-      ( ad.f.E.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * e.fawn.rate ) +
-      (juv.f.I.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * e.fawn.rate ) + 
-      ( ad.f.I.N * (1- hunt.mort.juv.f) * juv.repro * juv.f.an.sur * e.fawn.rate )
-    
-    # Juveniles 
-    juv.f.E.N <- (.5 * fawn.S.N * (1 - hunt.mort.fawn) * fawn.an.sur * e.fawn.rate) + 
-      (.5 * fawn.E.N * (1 - hunt.mort.fawn) * fawn.an.sur * (1 - i.fawn.rate))
-    
-    juv.m.E.N <- (.5 * fawn.S.N * (1 - hunt.mort.fawn) * fawn.an.sur * e.fawn.rate) + 
-      (.5 * fawn.E.N * (1 - hunt.mort.fawn) * fawn.an.sur * (1 - i.fawn.rate))
+    popout[t,3] <- .5 * popout[t-1,1] * (1 - hunt.fawn) * fawn.sur * (1 - e.rate)
     
     # Adults 
-    ad.f.E.N <- (ad.f.S.N * (1- hunt.mort.ad.f) * ad.an.f.sur * e.ja.rate) + 
-      (ad.f.E.N * (1 - hunt.mort.ad.f) * ad.an.f.sur * (1 - i.ja.rate))
+    popout[t,4] <- (popout[t-1,4] * (1- hunt.ad.f) * ad.f.sur * (1-e.rate)) + 
+      (popout[t-1,2] * (1 - hunt.juv.f) * juv.f.sur * (1-e.rate))
     
-    ad.m.E.N <- (ad.m.S.N * (1- hunt.mort.ad.m) * ad.an.m.sur * e.ja.rate) + 
-      (ad.m.E.N * (1 - hunt.mort.ad.m) * ad.an.m.sur * (1 - i.ja.rate))
+    popout[t,5] <- (popout[t-1,5] * (1- hunt.ad.m) * ad.m.sur * (1-e.rate)) + 
+      (popout[t-1, 3] * (1 - hunt.juv.m) * juv.m.sur * (1-e.rate))
     
+    # ### Exposed Category 
+    # # Fawns
+    popout[t,6] <- (popout[t-1,2] * (1- hunt.juv.f) * juv.repro.draw * juv.f.sur * (e.rate) ) + 
+      (popout[t-1,4] * (1- hunt.ad.f) * ad.repro.draw * ad.f.sur * (e.rate)) +
+      (popout[t-1,7] * (1- hunt.juv.f) * juv.repro.draw * juv.f.sur * (e.rate) ) + 
+      (popout[t-1,9] * (1- hunt.ad.f) * ad.repro.draw * ad.f.sur * (e.rate)) +
+      (popout[t-1,11] * (1- hunt.juv.f) * juv.repro.draw * juv.f.sur * (e.rate) ) + 
+      (popout[t-1,13] * (1- hunt.ad.f) * ad.repro.draw * ad.f.sur * (e.rate)) 
     
-    ### Infected Category 
+    # # Juveniles 
+    popout[t,7] <- .5 * popout[t-1,1] * (1 - hunt.fawn) * fawn.sur * e.rate + 
+      (.5 * popout[t-1,6] * (1 - hunt.fawn) * fawn.sur * (1 - i.rate))
     
-    # Juveniles 
-    juv.f.I.N <- (.5 * fawn.S.N * (1 - hunt.mort.fawn) * fawn.an.sur * i.ja.rate) 
+    popout[t,8] <- .5 * popout[t-1,1] * (1 - hunt.fawn) * fawn.sur * e.rate + 
+      (.5 * popout[t-1,6] * (1 - hunt.fawn) * fawn.sur * (1 - i.rate))
     
-    juv.m.I.N <- (.5 * fawn.S.N * (1 - hunt.mort.fawn) * fawn.an.sur * i.ja.rate)
+    # # Adults 
+    popout[t,9] <- (popout[t-1,4] * (1- hunt.ad.f) * ad.f.sur * e.rate) +
+      ( popout[t-1,9] * (1 - hunt.ad.f) * ad.f.sur * (1 - i.rate))
+
+    popout[t,10] <- (popout[t-1,5] * (1- hunt.ad.m) * ad.m.sur * e.rate) +
+      ( popout[t-1,10] * (1 - hunt.ad.m) * ad.m.sur * (1 - i.rate))
     
-    # Adults 
-    ad.f.I.N <- (ad.f.E.N * (1- hunt.mort.ad.f) * ad.an.f.sur * i.ja.rate) + 
-      (juv.f.E.N * (1 - hunt.mort.juv.f) * juv.f.an.sur * i.ja.rate) + 
-      (ad.f.I.N * (1- hunt.mort.ad.f) * ad.an.f.sur) + 
-      (juv.f.I.N * (1 - hunt.mort.juv.f) * juv.f.an.sur) 
+
+    ### Infected Category
+
+    # Juveniles
+    popout[t,11] <- (.5 * popout[t-1,6] * (1 - hunt.fawn) * fawn.sur * i.rate)
     
-    ad.m.I.N <- (ad.m.E.N * (1- hunt.mort.ad.m) * ad.an.m.sur * i.ja.rate) + 
-      (juv.m.E.N * (1 - hunt.mort.juv.m) * juv.m.an.sur * i.ja.rate) + 
-      (ad.m.I.N * (1- hunt.mort.ad.m) * ad.an.m.sur) + 
-      (juv.m.I.N * (1 - hunt.mort.juv.m) * juv.m.an.sur) 
+    popout[t,12] <- (.5 * popout[t-1,6] * (1 - hunt.fawn) * fawn.sur * i.rate)
     
+    # Adults
+    popout[t,13] <- (popout[t-1,9] * (1- hunt.ad.f) * ad.f.sur * i.rate) +
+      (popout[t-1,7] * (1 - hunt.juv.f) * juv.f.sur * i.rate) +
+      (popout[t-1,13] * (1- hunt.ad.f) * ad.f.sur) +
+      (popout[t-1,11] * (1 - hunt.juv.f) * juv.f.sur)
     
-    ### Update the environmental load
-    no.infect.indiv <- sum(juv.f.I.N, juv.m.I.N, ad.f.I.N, ad.m.I.N)
-    env <- env + (no.infect.indiv * shedrate) - (expdecayconstant * env)
+    popout[t,14] <- (popout[t-1,10] * (1- hunt.ad.m) * ad.m.sur * i.rate) +
+      (popout[t-1,8] * (1 - hunt.juv.m) * juv.m.sur * i.rate) +
+      (popout[t-1,14] * (1- hunt.ad.m) * ad.m.sur) +
+      (popout[t-1,12] * (1 - hunt.juv.m) * juv.m.sur)
     
-    ### Update e
-    # This needs work!!! [after fitted to susceptible data..]
-    no.susceptible.indiv <- sum(juv.f.S.N, juv.m.S.N, ad.f.S.N, ad.m.S.N)
-    # beta?? 
-    # e = beta * no.infect.indiv * no.susceptible.indiv + env
-    
-    # group age/sex/disease stage population for t + 1 into one output 
-    counts <- list(
-      fawn.S.N = fawn.S.N, juv.f.S.N = juv.f.S.N, juv.m.S.N = juv.m.S.N, ad.f.S.N = ad.f.S.N, ad.m.S.N = ad.m.S.N,
-      fawn.E.N = fawn.E.N, juv.f.E.N = juv.f.E.N, juv.m.E.N = juv.m.E.N, ad.f.E.N = ad.f.E.N, ad.m.E.N = ad.m.E.N,
-      juv.f.I.N = juv.f.I.N, juv.m.I.N = juv.m.I.N, ad.f.I.N = ad.f.I.N, ad.m.I.N = ad.m.I.N)
-    
-    popout[t,] <- unlist(counts)
-    
-    ## Need count of number of dead/removed individuals 
-    
-    hunted_indiv <- list(
-      fawn.S.N = fawn.S.N * hunt.mort.fawn, juv.f.S.N = juv.f.S.N * hunt.mort.juv.f, juv.m.S.N = juv.m.S.N * hunt.mort.juv.m, ad.f.S.N = ad.f.S.N * hunt.mort.ad.f, ad.m.S.N = ad.m.S.N * hunt.mort.ad.m,
-      fawn.E.N = fawn.E.N * hunt.mort.fawn, juv.f.E.N = juv.f.E.N * hunt.mort.juv.f, juv.m.E.N = juv.m.E.N * hunt.mort.juv.m, ad.f.E.N = ad.f.E.N * hunt.mort.ad.f, ad.m.E.N = ad.m.E.N * hunt.mort.ad.m,
-      juv.f.I.N = juv.f.I.N * hunt.mort.juv.f, juv.m.I.N = juv.m.I.N * hunt.mort.juv.m, ad.f.I.N = ad.f.I.N * hunt.mort.ad.f, ad.m.I.N = ad.m.I.N * hunt.mort.ad.m)
-    
-    hunted[t,] <- unlist(hunted_indiv)
+    # ### Update the environmental load
+    # no.infect.indiv <- sum(juv.f.I.N, juv.m.I.N, ad.f.I.N, ad.m.I.N)
+    # env <- env + (no.infect.indiv * shedrate) - (expdecayconstant * env)
+    # 
+    # ### Update e
+    # # This needs work!!! [after fitted to susceptible data..]
+    # no.susceptible.indiv <- sum(juv.f.S.N, juv.m.S.N, ad.f.S.N, ad.m.S.N)
+    # # beta?? 
+    # # e = beta * no.infect.indiv * no.susceptible.indiv + env
+
+
+    ## Need count of number of dead/removed individuals
+    hunted[t,] <- c(popout[t,1] * hunt.fawn, popout[t,2] * hunt.juv.f, popout[t,3] * hunt.juv.m, popout[t,4] * hunt.ad.f, popout[t,5] * hunt.ad.m, 
+                  popout[t,6] * hunt.fawn, popout[t,7] * hunt.juv.f, popout[t,8] * hunt.juv.m, popout[t,9] * hunt.ad.f, popout[t,10] * hunt.ad.m, 
+                                                popout[t,11] * hunt.juv.f, popout[t,12] * hunt.juv.m, popout[t,13] * hunt.ad.f, popout[t,14] * hunt.ad.m)
     
   } ## Close timeloop   
   
